589fcc6bc79172ef162710291aad129b67d8b71d
masashi iijima <masashiiijima@dcl.cs.waseda.ac.jp>
2019-03-11 18:04:26 +0900

    不要部分の削除

---
+++ ConcleteJungle/Assets/Scripts/Countdown.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Countdown : MonoBehaviour
+{
+    [SerializeField]
+    private Text _textCountdown;
+    private GameObject timer;
+    public GameObject start;
+    void Start()
+    {
+        timer = GameObject.Find("Timer");
+        _textCountdown.text = "";
+        //時を止める
+        Time.timeScale = 0f;
+
+        StartCoroutine(CountdownCoroutine());
+    }
+
+    IEnumerator CountdownCoroutine()
+    {
+        _textCountdown.text = "3";
+        yield return new WaitForSecondsRealtime(1.0f);
+
+        _textCountdown.text = "2";
+        yield return new WaitForSecondsRealtime(0.5f);
+
+        _textCountdown.text = "1";
+        yield return new WaitForSecondsRealtime(0.5f);
+
+
+        _textCountdown.text = "";
+        start.gameObject.SetActive(true);
+        yield return new WaitForSecondsRealtime(1.0f);
+
+        _textCountdown.gameObject.SetActive(false);
+        //時が動き出す
+        Time.timeScale = 1f;
+        timer.GetComponent<TimerScript>().enabled = true;
+    }
+}
---
+++ ConcleteJungle/Assets/Scripts/ResultDirector.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ResultDirector : MonoBehaviour {
+
+    public void SoloAgainButtonDown()
+    {
+		Debug.Log("Solo");
+        SceneManager.LoadScene("Solo");
+    }
+
+    public void SelectButtonDown()
+    {
+		Debug.Log("Select");
+        SceneManager.LoadScene("Select");
+    }
+
+    public void TitleButtonDown()
+    {
+		Debug.Log("Title");
+        SceneManager.LoadScene("Title");
+    }
+
+    public void EndButtonDown()
+    {
+		Debug.Log("End");
+        Application.Quit();
+    }
+}
